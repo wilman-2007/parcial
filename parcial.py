73906374dbe5a551be982284c322557aceee6163
wilman-2007 <wporto@unal.edu.co>
2025-10-16 17:20:33 -0500

    Add files via upload

--- parcial.py
+++ parcial.py
@@ -1,30 +1,29 @@
-class biblioteca():
-    def __init__(self, libro,usuario):
-      self.libro = libro
-      self._usuario = usuario 
-
-    
-
-
-      class usuario(biblioteca):
-        def __init__(self, nombre):
+class usuario():
+      def __init__(self, nombre):
               self.nombre = nombre 
-        def informacion_usuario(self):
-           return self.nombre   
+      def informacion_usuario(self):
+       return f"el usuario es: {self.nombre}"
 
-        def usuarios_nuevos(sel,usuario_nuevo):
+      def usuarios_nuevos(sel,usuario_nuevo):
            self.usuario_nuevo = usuario_nuevo
            if usuario_nuevo == usuario :
               raise TypeError("El usuario ya existe.")
            print("el usuario a sido aceptado correctamente. ")
            
-           
-        class libro (biblioteca):
-            def __init__(self,categoria):
-                self.categoria = categoria 
-                romance = libro("romance ")
+class libro ():
+       def __init__(self,categoria,titulo):
+        self.categoria = categoria
+        self.titulo = titulo
 
-            def nuevo_libro(self):
+       def infomacion_libro(self):
+           return f"categoria, {self.categoria}, libro, {self.titulo}"
+    
+      
+               
+class biblioteca():
+    def __init__(self, libro,usuario):
+      self.libro = libro
+      self._usuario = usuario                 
                
             
 
